8f63809bedf0eb816d9c2948548486eeeea4ec3d
Prashant Sinha <prashant@ducic.ac.in>
2016-01-14 20:05:46 +0530

    Exports 'make' routine to module level.

--- app/main.js
+++ app/main.js
@@ -3,6 +3,7 @@ var factory = require('./creature.js');
 
 module.exports = {
   Terrarium: Terrarium,
+  make: factory.make,
   registerCreature: factory.registerCreature,
   registerCA: factory.registerCA
 };
